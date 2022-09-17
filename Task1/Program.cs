f12edcc8059436fc3253e8a5ae55c9ce29c6c6f8
Tati <tanushka-p.94@mail.ru>
2022-09-17 22:37:28 +0300

    save1 task1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+
+
+void PrintNumberNmax(int N, int M)
+{
+    if( N < M) return;
+    PrintNumberNmax(N-1,M);
+    Console.Write(N+" ");
+}
+
+void PrintNumberMmax(int N, int M)
+{
+    if( M < N) return;
+    Console.Write(M+" ");
+    PrintNumberMmax(N,M-1);
+    
+}
+
+Console.WriteLine("Введите число M");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+
+
+PrintNumberNmax(N,M);
+
+PrintNumberMmax(N,M);
+ 
+
