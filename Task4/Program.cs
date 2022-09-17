a0d31cc5da6421a756edc8fa55c7b5e2fd439b26
Tati <tanushka-p.94@mail.ru>
2022-09-18 00:35:40 +0300

    save 4 task4

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления функции Аккермана
+int Akkerman(int m , int n)
+{
+    if (m == 0) return n+1;
+    else if (m > 0 && n == 0) return Akkerman(m-1,1);
+    else if (m > 0 && n > 0) return Akkerman(m-1,  Akkerman(m,n-1));
+    return Akkerman(m,n);
+}
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine($"Функция Аккермана : {Akkerman(m,n)}");
