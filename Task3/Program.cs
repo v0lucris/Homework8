f212bc31b35eafa9fd17828e8e9a455e8fd631b7
Tati <tanushka-p.94@mail.ru>
2022-09-18 00:02:08 +0300

    save3 task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму цифр числа
+
+
+
+int SumDigits(int number)
+{
+    if (number < 1) return 0;
+    int result = 0;
+    return SumDigits(number / 10) + number % 10;
+}
+Console.Write("Input number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Sum digits: {SumDigits(number)}");
