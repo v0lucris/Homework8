b238dcfce899342ade675cfb84ded443513dba45
Tati <tanushka-p.94@mail.ru>
2022-09-17 23:31:46 +0300

    save 2 task2

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+Console.WriteLine("Введите число M");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int n = int.Parse(Console.ReadLine());
+
+
+
+
+int Sum1(int m, int n)
+{
+    if (n == m ) return n;
+    return m + Sum1(m + 1, n);
+}
+
+
+int Sum2(int m, int n)
+{
+    if (n == m ) return n;
+    return m + Sum2(m - 1, n);
+}
+
+
+
+if (m < n) Console.WriteLine(Sum1(m, n));
+else Console.WriteLine(Sum2(m, n));
+
+
+
