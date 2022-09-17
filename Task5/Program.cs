d58d19a8c943390b2892b3790020f3793e43583e
Tati <tanushka-p.94@mail.ru>
2022-09-18 00:51:53 +0300

    save 5 task5

---
+++ Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+
+int DegreeNumber (int A, int B)
+{
+    if (B < 1 ) return 1;
+    return A * DegreeNumber ( A, B - 1);
+}
+Console.WriteLine("Input number A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Input number B");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($" А в степени В равно : {DegreeNumber ( A, B)}");
